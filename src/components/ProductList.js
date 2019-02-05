adeb5b25d55759eee3b87072b6b4b0f138979329
iTop <itop@192-168-0-103.rdsnet.ro>
2019-02-05 20:15:43 +0200

    add Context API to the app  (single source of truth)

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
 import Product from './Product';
 import Title from './Title';
+import {ProductConsumer} from '../context';
 
 export default class ProductList extends Component {
-    state ={
-        products: []
-    }
 
     render() {
+        console.log(this.state.products);
         return (
             <React.Fragment>
                 <div className="py-5">
                     <div className="container">
                             <Title name={'our'} title={'products'} />
+                            <div className="row">
+                                <ProductConsumer>
+                                    {(value)=>{
+                                        return <h1>{value}</h1>
+                                    }}
+                                </ProductConsumer>
+                            </div>
                     </div>
                 </div>
                 {/* <Product /> */}
