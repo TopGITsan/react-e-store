33cd9d6f81ebe0323cda222e97031eca3f9bf687
iTop <itop@192-168-0-103.rdsnet.ro>
2019-02-05 15:05:29 +0200

    create components and add react-router-dom to npm

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
 
 class App extends Component {
 	render() {
 		return (
-			<div className='container'>
-				<div className='row'>
-					<div className='col-6'>Column number 1</div>
-					<div className='col-6'><span><i className='fas fa-home'></i></span></div>
-				</div>
-			</div>
+			<React.Fragment>
+				<h3 className='text-center'>hello from App</h3>
+				<Navbar />
+				<ProductList />
+				<Details />
+				<Cart />
+				<Default />
+			</React.Fragment>
 		);
 	}
 }
