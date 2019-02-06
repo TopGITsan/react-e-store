723e6f46383af912d94ca53bba68f2cea9672668
iTop <itop@192-168-0-103.rdsnet.ro>
2019-02-06 13:48:47 +0200

    add props checks and styles in ButtonContainer

--- src/components/Details.js
+++ src/components/Details.js
@@ -36,8 +36,11 @@ export default class Details extends Component {
                                         <Link to='/'>
                                             <ButtonContainer>Back to products</ButtonContainer>
                                         </Link>
-                                        <ButtonContainer cart disabled={inCart?true:false} onClick={()=>{
-                                            value.addToCart(id)
+                                        <ButtonContainer 
+                                            cart 
+                                            disabled={inCart?true:false} 
+                                            onClick={()=>{
+                                                value.addToCart(id)
                                         }}>
                                             {inCart?'inCart':'add to Cart'}
                                         </ButtonContainer>
