b97c2fb0de8e526e24ac28e880a097002189a3d8
iTop <itop@192-168-0-103.rdsnet.ro>
2019-02-07 20:13:04 +0200

    render and pass props to CartList

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,6 +4,7 @@ import Title from "../Title";
 import CartColumns from './CartColumns';
 import EmptyCart from "./EmptyCart";
 import { ProductConsumer } from "../../context";
+import CartList from './CartList'; 
 
 export default class Cart extends Component {
 	render() {
@@ -12,11 +13,12 @@ export default class Cart extends Component {
 				<ProductConsumer>
 					{value=>{
 						const {cart} = value;
-						if (cart.length>0){
+						if (!cart.length>0){
 							return (
 								<React.Fragment>
 									<Title name='your' title='cart' />
 									<CartColumns />
+									<CartList value={value}/>
 								</React.Fragment>
 							);
 						} else {
---
+++ src/components/Cart/CartItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CartItem() {
+    return (
+        <div>
+            <h5>this is a cart item</h5>
+        </div>
+    )
+}
