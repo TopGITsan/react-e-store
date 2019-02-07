060de350d1f6411f4315ab0252e4685f9c3d6e1b
iTop <itop@192-168-0-103.rdsnet.ro>
2019-02-07 18:59:49 +0200

    setup the Cart comp functionality into the Cart folder

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import { Title } from "../Title";
+
 
 export default class Cart extends Component {
 	render() {
 		return (
-			<div>
+			<section>
 				<h3>Hello from Cart</h3>
-			</div>
+			</section>
 		);
 	}
 }
