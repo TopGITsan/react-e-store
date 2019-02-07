50a8ea27e130a9661be3cb61733226866ec6ec8e
iTop <itop@192-168-0-103.rdsnet.ro>
2019-02-07 18:03:12 +0200

    call the functionality of the modal when clicking on cart button

--- src/components/Details.js
+++ src/components/Details.js
@@ -40,7 +40,8 @@ export default class Details extends Component {
                                             cart 
                                             disabled={inCart?true:false} 
                                             onClick={()=>{
-                                                value.addToCart(id)
+                                                value.addToCart(id);
+                                                value.openModal(id);
                                         }}>
                                             {inCart?'inCart':'add to Cart'}
                                         </ButtonContainer>
