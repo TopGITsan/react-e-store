4f7f285ca6ddfe49a05d0ed9ce855505831c6b8b
iTop <itop@192-168-0-103.rdsnet.ro>
2019-02-08 19:24:42 +0200

    fix a bug in Modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -22,7 +22,7 @@ export default class Modal extends Component {
                                     <div className="row">
                                         <div id='modal' className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize p-5">
                                             <h5>item added to the cart</h5>
-                                            <img src={img} alt="product" className='image-fluid'/>
+                                            <img src={img} alt="product" className='img-fluid'/>
                                             <h5>{title}</h5>
                                             <h5 className="text-muted">price: ${price}</h5>
                                             <Link to='/'>
