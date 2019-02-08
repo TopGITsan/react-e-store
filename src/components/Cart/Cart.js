751a50c82088132126b66960c0176f91c4e432ac
iTop <itop@192-168-0-103.rdsnet.ro>
2019-02-08 19:24:07 +0200

    add CartItem UI + styles

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -5,6 +5,7 @@ import CartColumns from './CartColumns';
 import EmptyCart from "./EmptyCart";
 import { ProductConsumer } from "../../context";
 import CartList from './CartList'; 
+import CartTotals from './CartTotals';
 
 export default class Cart extends Component {
 	render() {
@@ -13,12 +14,13 @@ export default class Cart extends Component {
 				<ProductConsumer>
 					{value=>{
 						const {cart} = value;
-						if (!cart.length>0){
+						if (cart.length>0){
 							return (
 								<React.Fragment>
 									<Title name='your' title='cart' />
 									<CartColumns />
 									<CartList value={value}/>
+									<CartTotals value={value}/>
 								</React.Fragment>
 							);
 						} else {
